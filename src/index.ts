2f506ae190735cc2c4ef49d3c984c7c32951a8ba
Mitch Crowe <crowe.mitch@gmail.com>
2017-04-23 18:06:34 -0700

    Improve reporting

--- src/index.ts
+++ src/index.ts
@@ -15,17 +15,21 @@ interface ISchema {
 function run() {
   const schema = loadSchema()
 
-  let isValid = true
+  const invalid = []
 
   for (let name in schema.required) {
     if (!process.env[name] || process.env[name].length == 0) {
       const description = schema.required[name]
-      console.error(`Missing required environment variable "${name}": ${description}`)
-      isValid = false
+      invalid.push({ name, description })
     }
   }
 
-  if (!isValid) {
+  if (invalid.length > 0) {
+    console.log('EnvCheck: Missing the following required environment variables:')
+    for (let variable of invalid) {
+      console.log(`${variable.name}: ${variable.description}`)
+    }
+    console.log('Aborting...')
     process.exit()
   }
 }
@@ -38,12 +42,12 @@ function loadSchema(): ISchema {
   const schema = JSON.parse(fs.readFileSync('package.json', 'utf-8')).env
 
   if (!schema) {
-    console.error('Missing "env" key in package.json')
+    console.error('EnvCheck: Missing "env" key in package.json. Aborting...')
     process.exit()
   }
 
   if (!schema.required && !schema.optional) {
-    console.error('"env" key in package.json must have a "required" or "optional" section')
+    console.error('EnvCheck: "env" key in package.json must have a "required" or "optional" section. Aborting...')
     process.exit()
   }
 
